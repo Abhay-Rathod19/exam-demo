b8852d1e79edd3ef57d0fd81419f7fe1449c7944
abhay-rathod <abhayr.tagline@gmail.com>
2024-06-18 14:43:34 +0530

    Fix protected routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Message } from "./containers/Message";
 import { UserRoutes } from "./routes/userRoutes";
 
 function App() {
   return (
     <div className="main-container">
- 
       <UserRoutes />
     </div>
   );
--- src/routes/private/PrivateRoute.js
+++ src/routes/private/PrivateRoute.js
@@ -1,11 +1,7 @@
 import { Navigate } from "react-router";
 import { authenticateUser } from "../../helpers/authentication";
 import { DashBoardComp } from "../../presentation/DashBoard";
-
-const userRouteRole = {
-  teacher: 1,
-  student: 1,
-};
+import { areEqual } from "../../utils/javaScript";
 
 export const PrivateRoute = ({ routeRole }) => {
   const lcStrRole = authenticateUser();
@@ -13,7 +9,7 @@ export const PrivateRoute = ({ routeRole }) => {
   if (!routeRole) {
     return <Navigate to="/" />;
   }
-  if (userRouteRole[lcStrRole]) {
+  if (routeRole && areEqual(routeRole, lcStrRole)) {
     return <DashBoardComp role={lcStrRole} />;
   }
 
